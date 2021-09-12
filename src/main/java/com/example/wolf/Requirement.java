b1cd8f533c5e92f21724da3c5e6b7c5b88160aa9
awolf1972 <new.volkov@gmail.com>
2021-09-13 00:13:15 +0300

    Upgrade Apache POI version, copying sheets to result. Intermediate release

---
+++ src/main/java/com/example/wolf/Requirement.java
@@ -0,0 +1,107 @@
+package com.example.wolf;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+
+public class Requirement {
+
+    String id; // Requirement id
+    int level; // Requirement level
+    String name; // Requirement
+    String priority; // Requirement priority
+    String done; // Requirement has realised
+    String reference; // Requirement from other source (MarxxWeb)
+    String integration; // Integration requirement
+    String comment; // Comment for requirement
+    String linked; // Linked requirement
+    String version; // Plan to realised in version
+    String release; // Plan to realized in release
+    String questions; // Work questions for requirement
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Requirement that = (Requirement) o;
+
+        if (level != that.level) return false;
+        if (!id.equals(that.id)) return false;
+        if (!name.equals(that.name)) return false;
+        if (priority != null ? !priority.equals(that.priority) : that.priority != null) return false;
+        if (done != null ? !done.equals(that.done) : that.done != null) return false;
+        if (reference != null ? !reference.equals(that.reference) : that.reference != null) return false;
+        if (integration != null ? !integration.equals(that.integration) : that.integration != null) return false;
+        if (comment != null ? !comment.equals(that.comment) : that.comment != null) return false;
+        if (linked != null ? !linked.equals(that.linked) : that.linked != null) return false;
+        if (version != null ? !version.equals(that.version) : that.version != null) return false;
+        if (release != null ? !release.equals(that.release) : that.release != null) return false;
+        return questions != null ? questions.equals(that.questions) : that.questions == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + level;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (priority != null ? priority.hashCode() : 0);
+        result = 31 * result + (done != null ? done.hashCode() : 0);
+        result = 31 * result + (reference != null ? reference.hashCode() : 0);
+        result = 31 * result + (integration != null ? integration.hashCode() : 0);
+        result = 31 * result + (comment != null ? comment.hashCode() : 0);
+        result = 31 * result + (linked != null ? linked.hashCode() : 0);
+        result = 31 * result + (version != null ? version.hashCode() : 0);
+        result = 31 * result + (release != null ? release.hashCode() : 0);
+        result = 31 * result + (questions != null ? questions.hashCode() : 0);
+        return result;
+    }
+
+    /**
+     * Fills object fields from XLSX row
+     * @param row - Excel XLSX row
+     */
+    public void loadFromRow(XSSFRow row) {
+        try {
+            int cells = row.getLastCellNum();
+
+            if (cells > 0) level = (int) row.getCell(0).getNumericCellValue(); // Requirement level
+            if (cells > 1) name = row.getCell(1).getStringCellValue(); // Requirement
+            if (cells > 2) priority = row.getCell(2).getStringCellValue(); // Requirement priority
+            if (cells > 3) done = row.getCell(3).getStringCellValue(); // Requirement has realised
+            if (cells > 4) reference = row.getCell(4).getStringCellValue(); // Requirement from other source (MarxxWeb)
+
+            if (cells > 6) integration = row.getCell(6).getStringCellValue(); // Integration requirement
+            if (cells > 7) comment = row.getCell(7).getStringCellValue(); // Comment for requirement
+            if (cells > 8) linked = row.getCell(8).getStringCellValue(); // Linked requirement
+
+            if (cells > 13) version = row.getCell(13).getStringCellValue(); // Plan to realised in version
+            if (cells > 14) release = row.getCell(14).getStringCellValue(); // Plan to realized in release
+            if (cells > 15) questions = row.getCell(15).getStringCellValue(); // Work questions for requirement
+        }
+        catch (Exception e) {
+            System.out.println("Error while loading row " + row.getRowNum());
+        }
+    }
+
+    /**
+     * Fills XLSX row from object
+     * @param row - XLSX row
+     */
+    public void saveToRow(XSSFRow row) {
+        XSSFCell cell = row.createCell(0); cell.setCellValue(level); // Requirement level
+        cell = row.createCell(1); cell.setCellValue(name); // Requirement
+        cell = row.createCell(2); cell.setCellValue(priority); // Requirement priority
+        cell = row.createCell(3); cell.setCellValue(done); // Requirement has realised
+        cell = row.createCell(4); cell.setCellValue(reference); // Requirement from other source (MarxxWeb)
+
+        cell = row.createCell(6); cell.setCellValue(integration); // Integration requirement
+        cell = row.createCell(7); cell.setCellValue(comment); // Comment for requirement
+        cell = row.createCell(8); cell.setCellValue(linked); // Linked requirement
+
+        cell = row.createCell(13); cell.setCellValue(version); // Plan to realised in version
+        cell = row.createCell(14); cell.setCellValue(release); // Plan to realized in release
+        cell = row.createCell(15); cell.setCellValue(questions); // Work questions for requirement
+    }
+
+}
--- src/main/java/com/example/wolf/XLSCompareMain.java
+++ src/main/java/com/example/wolf/XLSCompareMain.java
@@ -1,6 +1,8 @@
 package com.example.wolf;
 
+import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.*;
 
 import java.io.FileInputStream;
@@ -11,6 +13,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class XLSCompareMain {
+
     public static void main (String[] args) {
         System.out.println("Trying to compare XLSX with hierarchy rows.");
 
@@ -33,6 +36,12 @@ public class XLSCompareMain {
         doCompare(sampleName, checkName, resultName);
     }
 
+    /**
+     * Compare procedure
+     * @param sampleName - source file name
+     * @param checkName - file name for compare
+     * @param resultName - file name for result
+     */
     private static void doCompare(String sampleName, String checkName, String resultName) {
 
         String fileName = "?";
@@ -40,36 +49,36 @@ public class XLSCompareMain {
 
             fileName = sampleName;
             System.out.println("Loading sample file " + fileName);
-            LinkedHashMap<String, String> sample = readFromExcel(fileName);
+            LinkedHashMap<String, Requirement> sample = readFromExcel(fileName);
             System.out.println("Rows loaded: " + sample.size());
 
             fileName = checkName;
             System.out.println("Loading check file " + fileName);
-            LinkedHashMap<String, String> check = readFromExcel(fileName);
+            LinkedHashMap<String, Requirement> check = readFromExcel(fileName);
             System.out.println("Rows loaded: " + check.size());
 
-            LinkedHashMap<String, String> deleted = new LinkedHashMap<>();
-            LinkedHashMap<String, String> added = new LinkedHashMap<>();
+            LinkedHashMap<String, Requirement> deleted = new LinkedHashMap<>();
+            LinkedHashMap<String, Requirement> added = new LinkedHashMap<>();
 
-            for (Map.Entry<String, String> item : sample.entrySet()) {
+            for (Map.Entry<String, Requirement> item : sample.entrySet()) {
                 String key = item.getKey();
-                String value = item.getValue();
+                Requirement req = item.getValue();
                 if (!check.containsKey(key)) {
-                    deleted.put(key, value);
+                    deleted.put(key, req);
                 }
             }
 
-            for (Map.Entry<String, String> item : check.entrySet()) {
+            for (Map.Entry<String, Requirement> item : check.entrySet()) {
                 String key = item.getKey();
-                String value = item.getValue();
+                Requirement req = item.getValue();
                 if (!sample.containsKey(key)) {
-                    added.put(key, value);
+                    added.put(key, req);
                 }
             }
 
             System.out.println("Compared.");
 
-            outResult(resultName, added, deleted);
+            outResult(sampleName, checkName, resultName, sample, check, added, deleted);
 
         }
         catch (IOException e) {
@@ -78,12 +87,18 @@ public class XLSCompareMain {
 
     }
 
-    private static LinkedHashMap<String, String> readFromExcel(String file) throws IOException{
+    /**
+     * Reads one Excel file (first sheet)
+     * @param file - file name
+     * @return - array with sheet data
+     * @throws IOException
+     */
+    private static LinkedHashMap<String, Requirement> readFromExcel(String file) throws IOException{
 
-        LinkedHashMap<String, String> array = new LinkedHashMap<>();
+        LinkedHashMap<String, Requirement> array = new LinkedHashMap<>();
 
-        XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream(file));
-        XSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
+        XSSFWorkbook book = new XSSFWorkbook(new FileInputStream(file));
+        XSSFSheet sheet = book.getSheetAt(0);
 
         int rowNum = 2; // Start row (skip header)
 
@@ -94,7 +109,7 @@ public class XLSCompareMain {
 
         do {
 
-            XSSFRow row = myExcelSheet.getRow(rowNum);
+            XSSFRow row = sheet.getRow(rowNum);
             if (row == null) break;
 
             int level = 0;
@@ -102,11 +117,11 @@ public class XLSCompareMain {
 
             String currentPath = printPath(path);
 
-            if (row.getCell(0).getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
+            if (row.getCell(0).getCellType() == CellType.NUMERIC) {
                 level = (int) row.getCell(0).getNumericCellValue();
             }
 
-            if (row.getCell(1).getCellType() == XSSFCell.CELL_TYPE_STRING) {
+            if (row.getCell(1).getCellType() == CellType.STRING) {
                 name = row.getCell(1).getStringCellValue();
             }
 
@@ -135,7 +150,10 @@ public class XLSCompareMain {
                 currentPath = printPath(path);
             }
 
-            array.put(currentPath + "|" + name, name);
+            Requirement req = new Requirement();
+            req.id = currentPath + "|" + name;
+            req.loadFromRow(row);
+            array.put(req.id, req);
 
 //            System.out.println("Row: " + rowNum + ": " + level + ": " + name + " | " + currentPath);
 
@@ -148,80 +166,128 @@ public class XLSCompareMain {
         }
         while (true);
 
-        myExcelBook.close();
+        book.close();
 
         return array;
 
     }
 
-    private static void outResult(String fileName, LinkedHashMap<String, String> added, LinkedHashMap<String, String> deleted) {
+    /**
+     * Outputs compare results
+     * @param sampleFileName - sample file name
+     * @param checkFileName - file name for check
+     * @param resultFileName - file name for results
+     * @param sample - array with source data
+     * @param check - array with data for compare
+     * @param added - array with added rows
+     * @param deleted - array with deleted rows
+     */
+    private static void outResult(String sampleFileName, String checkFileName, String resultFileName,
+                                  LinkedHashMap<String, Requirement> sample, LinkedHashMap<String, Requirement> check,
+                                  LinkedHashMap<String, Requirement> added, LinkedHashMap<String, Requirement> deleted) {
 
-        Workbook book = new XSSFWorkbook();
+        XSSFWorkbook book = new XSSFWorkbook();
 
         if (added.size() > 0 || deleted.size() > 0) {
 
             if (added.size() > 0) {
-                Sheet sheet = book.createSheet("Added");
                 System.out.println("+++ Added rows: " + added.size() + " +++");
-                outOneSheet(sheet, added);
+                XSSFSheet sampleSheet = book.createSheet("Old");
+                copySheet(sampleFileName, sampleSheet);
+                XSSFSheet addSheet = book.createSheet("Added");
+                outOneDiffSheet(addSheet, added);
             } else {
                 System.out.println("There are no added rows.");
             }
 
             if (deleted.size() > 0) {
-                Sheet sheet = book.createSheet("Deleted");
+                XSSFSheet checkSheet = book.createSheet("New");
+                copySheet(checkFileName, checkSheet);
+                XSSFSheet sheet = book.createSheet("Deleted");
                 System.out.println("--- Deleted rows: " + deleted.size() + "---");
-                outOneSheet(sheet, deleted);
+                outOneDiffSheet(sheet, deleted);
             } else {
                 System.out.println("There are no deleted rows.");
             }
             try {
-                book.write(new FileOutputStream(fileName));
+                book.write(new FileOutputStream(resultFileName));
                 book.close();
             }
             catch (IOException e) {
-                System.out.println("Error saving file: " + fileName);
+                System.out.println("Error saving file: " + resultFileName);
             }
         }
     }
 
-    private static void outOneSheet(Sheet sheet, LinkedHashMap<String, String> array) {
-        CellStyle style = sheet.getWorkbook().createCellStyle();
-        Font font = sheet.getWorkbook().createFont();
+    /**
+     * Outputs one source sheet (sample or check)
+     * @param sheet - sheet
+     * @param array - array to fill sheet
+     */
+    private static void outOneSheet(XSSFSheet sheet, LinkedHashMap<String, Requirement> array) {
+        int rowNum = 0;
+        for (Map.Entry<String, Requirement> item : array.entrySet()) {
+            XSSFRow row = sheet.createRow(rowNum);
+            item.getValue().saveToRow(row);
+            rowNum++;
+        }
+    }
+
+    /**
+     * Outputs one different sheet (added or deleted rows)
+     * @param sheet - sheet
+     * @param array - array to fill sheet
+     */
+    private static void outOneDiffSheet(XSSFSheet sheet, LinkedHashMap<String, Requirement> array) {
+        XSSFCellStyle style = sheet.getWorkbook().createCellStyle();
+        XSSFFont font = sheet.getWorkbook().createFont();
         font.setColor(IndexedColors.GREY_50_PERCENT.getIndex());
         style.setFont(font);
 
         int rowNum = 0;
         int level = 0;
         String oldPathStr = "";
-        for (Map.Entry<String, String> item : array.entrySet()) {
+        String[] oldPathArr = {};
+        for (Map.Entry<String, Requirement> item : array.entrySet()) {
             String pathStr = getPath(item.getKey());
             if (!pathStr.equals(oldPathStr)) {
                 String[] pathArr = pathStr.split("\\\\");
                 level = 0;
                 for (int i = 1; i < pathArr.length; i++) {
-                    Row pathRow = sheet.createRow(rowNum);
-                    Cell pathCount = pathRow.createCell(0);
+/*
+                    if (i < oldPathArr.length) { // Do not output common parts of path
+                        if (oldPathArr[i].equals(pathArr[i])) continue;
+                    }
+*/
+                    XSSFRow pathRow = sheet.createRow(rowNum);
+                    XSSFCell pathCount = pathRow.createCell(0);
+                    pathCount.setCellType(CellType.NUMERIC);
                     pathCount.setCellStyle(style);
                     pathCount.setCellValue(i);
-                    Cell pathHead = pathRow.createCell(1);
+                    XSSFCell pathHead = pathRow.createCell(1);
                     pathHead.setCellStyle(style);
                     pathHead.setCellValue(pathArr[i]);
                     rowNum++;
                     level = i;
                 }
                 oldPathStr = pathStr;
+                oldPathArr = pathArr;
             }
-            Row row = sheet.createRow(rowNum);
-            Cell path = row.createCell(0);
+            XSSFRow row = sheet.createRow(rowNum);
+            XSSFCell path = row.createCell(0);
             path.setCellValue(level + 1);
-            Cell name = row.createCell(1);
+            XSSFCell name = row.createCell(1);
             name.setCellValue(getName(item.getKey()));
             rowNum++;
             // System.out.println(path + " > " + name);
         }
     }
 
+    /**
+     * Builds path string from array with hierarchical rows
+     * @param path - array with path
+     * @return - string with path divided by "\"
+     */
     private static String printPath(ArrayList<String> path) {
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < path.size(); i++) {
@@ -234,6 +300,11 @@ public class XLSCompareMain {
         return str.toString();
     }
 
+    /**
+     * Returns name from full path (path with name), divided by "|"
+     * @param item - path with name
+     * @return - only name
+     */
     private static String getName(String item) {
         String name = "";
         if (item != null) {
@@ -243,6 +314,11 @@ public class XLSCompareMain {
         return name;
     }
 
+    /**
+     * Returns path from full path (path with name), divided by "|"
+     * @param item - path with name
+     * @return - only path
+     */
     private static String getPath(String item) {
         String name = "";
         if (item != null) {
@@ -251,4 +327,113 @@ public class XLSCompareMain {
         }
         return name;
     }
+
+    private static void copySheet(String sourceFile, XSSFSheet targetSheet) {
+
+        try {
+
+            XSSFWorkbook sourceBook = new XSSFWorkbook(new FileInputStream(sourceFile));
+            XSSFSheet sourceSheet = sourceBook.getSheetAt(0);
+            int oldOutlineLevel = 0;
+            int lastRow = sourceSheet.getLastRowNum();
+            for (int i = 0; i < lastRow; i++) {
+                int outlineLevel = sourceSheet.getRow(i).getOutlineLevel();
+                System.out.println("Copying row " + i + " Outline: " + outlineLevel);
+                copyRow(sourceSheet, targetSheet, i, i);
+            }
+
+            sourceBook.close();
+
+        }
+        catch (IOException e) {
+            System.out.println("Error while reading source sheet: " + sourceFile);
+        }
+
+    }
+
+    private static void copyRow(XSSFSheet sourceWorksheet, XSSFSheet targetWorksheet,
+                                int sourceRowNum, int targetRowNum) {
+        // Get the source / new row
+        XSSFRow newRow = targetWorksheet.getRow(targetRowNum);
+        XSSFRow sourceRow = sourceWorksheet.getRow(sourceRowNum);
+
+        // If the row exist in destination, push down all rows by 1 else create a new row
+        if (newRow != null) {
+            targetWorksheet.shiftRows(targetRowNum, targetWorksheet.getLastRowNum(), 1);
+        } else {
+            newRow = targetWorksheet.createRow(targetRowNum);
+        }
+
+        // Loop through source columns to add to new row
+        for (int i = 0; i < sourceRow.getLastCellNum(); i++) {
+            // Grab a copy of the old/new cell
+            XSSFCell oldCell = sourceRow.getCell(i);
+            XSSFCell newCell = newRow.createCell(i);
+
+            // If the old cell is null jump to next cell
+            if (oldCell == null) {
+                newCell = null;
+                continue;
+            }
+
+            // Copy style from old cell and apply to new cell
+            XSSFCellStyle newCellStyle = targetWorksheet.getWorkbook().createCellStyle();
+            newCellStyle.cloneStyleFrom(oldCell.getCellStyle());
+
+            newCell.setCellStyle(newCellStyle);
+            targetWorksheet.setColumnWidth(i, sourceWorksheet.getColumnWidth(i));
+
+
+            // If there is a cell comment, copy
+            if (oldCell.getCellComment() != null) {
+                newCell.setCellComment(oldCell.getCellComment());
+            }
+
+            // If there is a cell hyperlink, copy
+            if (oldCell.getHyperlink() != null) {
+                newCell.setHyperlink(oldCell.getHyperlink());
+            }
+
+            // Set the cell data value
+            CellType type = oldCell.getCellType();
+            if (type == CellType.BLANK) {
+                newCell.setCellType(type);
+                newCell.setCellValue(oldCell.getStringCellValue());
+            }
+            else if (type == CellType.BOOLEAN) {
+                newCell.setCellType(type);
+                newCell.setCellValue(oldCell.getBooleanCellValue());
+            }
+            else if (type == CellType.ERROR) {
+                newCell.setCellType(type);
+                newCell.setCellErrorValue(oldCell.getErrorCellValue());
+            }
+            else if (type == CellType.FORMULA) {
+                newCell.setCellFormula(oldCell.getCellFormula());
+            }
+            else if (type == CellType.NUMERIC) {
+                newCell.setCellType(type);
+                newCell.setCellValue(oldCell.getNumericCellValue());
+            }
+            else if (type == CellType.STRING) {
+                newCell.setCellType(type);
+                newCell.setCellValue(oldCell.getRichStringCellValue());
+            }
+        }
+
+        // If there are any merged regions in the source row, copy to new row
+        for (int i = 0; i < sourceWorksheet.getNumMergedRegions(); i++) {
+            CellRangeAddress cellRangeAddress = sourceWorksheet.getMergedRegion(i);
+            if (cellRangeAddress.getFirstRow() == sourceRow.getRowNum()) {
+                CellRangeAddress newCellRangeAddress = new CellRangeAddress(newRow.getRowNum(),
+                        (newRow.getRowNum() +
+                                (cellRangeAddress.getLastRow() - cellRangeAddress.getFirstRow()
+                                )),
+                        cellRangeAddress.getFirstColumn(),
+                        cellRangeAddress.getLastColumn());
+                targetWorksheet.addMergedRegion(newCellRangeAddress);
+            }
+        }
+    }
+
 }
